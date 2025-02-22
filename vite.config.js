d6db35bf61d7b444b8d5b724dc3986edbf2c865e
temppone <gtvtempone@gmail.com>
2025-02-22 17:24:44 -0300

    feat: initial packages config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    "@": path.resolve(dirname(fileURLToPath(import.meta.url)), "./src"),
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
